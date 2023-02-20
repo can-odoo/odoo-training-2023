9bb3d2fbf75965bacb1c024f674a7d10d236c419
radh-odoo <radh@odoo.com>
2023-02-20 14:34:36 +0530

    ui

--- buildproducts/models/build_products.py
+++ buildproducts/models/build_products.py
@@ -3,13 +3,23 @@ from odoo import models, fields
 class BuildProducts(models.Model):
     _name = "build.products"
     _description = "Build Products"
+     
 
     name = fields.Char('Product Name' , required=True)
     description = fields.Char('Description', required=True)
     product_type = fields.Selection(selection=[('opc', 'OPC'), ('ppc', 'PPC')])
     product_grade = fields.Selection(selection=[('33', '33'), ('43', '43'), ('53', '53')])
-    date_order = fields.Date('Date of Order')
     product_price = fields.Float('Product Price')
     product_quantity = fields.Integer('Product Quantity')
     total_price = fields.Float('Total Price')
-    delivery_date = fields.Date('Expected Delivery Date')
+    date_order = fields.Date('Date of Order')
+    delivery_date = fields.Date('Expected Delivery Date' , default=fields.Date.add(fields.Date.today(), days=2))
+    state = fields.Selection([
+        ('new', 'New'),
+        ('oder_accepted', 'Order Accepted'),
+        ('in_progress', 'In Progress'),
+        ('in_delivery', 'In Delivery'),
+        ('delivered', 'Delivered'),
+        ('canceled', 'Canceled')
+    ], string='State', default='new')
+    active = fields.Boolean(string='Active', default=True)
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,13 +7,21 @@ class EstateProperty(models.Model):
     name = fields.Char('Property Name' , required=True)
     description = fields.Char('Description', required=True)
     postcode = fields.Char('Postcode' , required=True)
-    date_availability = fields.Date('Date of Availability')
+    date_availability = fields.Date('Date of Availability' , default=fields.Date.add(fields.Date.today(),months=3) , copy=False)
     expected_price = fields.Float('Expected Price')
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('No. of Bedrooms' , required=True)
+    selling_price = fields.Float('Selling Price' , readonly=True , copy=False)
+    bedrooms = fields.Integer('No. of Bedrooms' , default=2)
     living_area = fields.Integer('Living Area (sqm)' , required=True)
     facades = fields.Integer('Facades' , required=True)
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden' )
     garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(selection=[('north', 'North'), ('south','South'), ('west','West'), ('east', 'East')])
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_recieved', 'Offer Recieved'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], string='State', default='new')
+    active = fields.Boolean(string='Active' , default=True)
