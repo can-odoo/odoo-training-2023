47bd9ed3add8521121ba01c9c9532d721106d51d
divy-odoo <divy@odoo.com>
2023-02-21 17:58:02 +0530

    estate & franchise : added search & form view

--- franchise_ERP/models/franchise_property.py
+++ franchise_ERP/models/franchise_property.py
@@ -4,15 +4,17 @@ class FranchiseProperty(models.Model):
     _name = "franchise.property"
     _description = "This is showroom property module"
 
-    name = fields.Char("Name of the firm")
+    name = fields.Char("Firm")
     franchise_id = fields.Integer(required=True)
     franchise_owner = fields.Char(required=True)
     no_of_product = fields.Integer(default=0)
+    location = fields.Char()
     opening_date = fields.Date(default = fields.Date.today)
     total_sale = fields.Float(copy=False)
     total_revanue = fields.Float("Total revanue :",readonly=True,default=True,copy=False) 
-    phone_no =fields.Char()
+    phone_no =fields.Char(size=10)
     type_franchise = fields.Selection(selection = [('job franchise','job franchise'),('product franchise','product franchise'),('business format franchise','business format franchise'),('investment franchise','investment franchise'),('conversion franchise','conversion franchise')])
+    active = fields.Boolean(active=True)
 
 
 
