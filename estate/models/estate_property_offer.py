11be92e65c4c5a8e357ee4c2c2d7ac61007e22ee
divy-odoo <divy@odoo.com>
2023-02-22 18:54:42 +0530

    [Chapter 8] added dob

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -8,3 +8,4 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(selection =[('accepted','Accepted'),('refused','Refused')], copy=False)
     partner_id = fields.Many2one('res.partner',required = True)
     property_id = fields.Many2one('estate.property', required=True)
+    dob = fields.Integer(string="DOB")
