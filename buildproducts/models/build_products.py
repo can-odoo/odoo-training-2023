ff3466c5c35646d8d37cad398f071cb3fd8bda43
radh-odoo <radh@odoo.com>
2023-02-23 18:13:01 +0530

    [Chapter 8]: removed certain fields which needed ORM

--- buildproducts/models/build_products.py
+++ buildproducts/models/build_products.py
@@ -10,15 +10,6 @@ class BuildProducts(models.Model):
     product_grade = fields.Selection(selection=[('33', '33'), ('43', '43'), ('53', '53')])
     product_price = fields.Float('Product Price')
     product_quantity = fields.Integer('Product Quantity')
-    total_price = fields.Float('Total Price')
-    date_order = fields.Date('Date of Order')
-    delivery_date = fields.Date('Expected Delivery Date' , default=fields.Date.add(fields.Date.today(), days=2))
-    state = fields.Selection([
-        ('new', 'New'),
-        ('oder_accepted', 'Order Accepted'),
-        ('in_progress', 'In Progress'),
-        ('in_delivery', 'In Delivery'),
-        ('delivered', 'Delivered'),
-        ('canceled', 'Canceled')
-    ], string='State', default='new')
     active = fields.Boolean(string='Active', default=True)
+    order_ids = fields.One2many('build.products.order', 'product_id', string='Orders')
+    tag_ids = fields.Many2many('build.products.tag' , string='Product Tags')
