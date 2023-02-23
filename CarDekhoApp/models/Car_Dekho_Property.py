15fd1bca6f15d94eaba1abdb59bfb35333d934c2
utag-odoo <utag@odoo.com>
2023-02-23 18:22:26 +0530

    some fields added that shows relationship between models

--- CarDekhoApp/models/Car_Dekho_Property.py
+++ CarDekhoApp/models/Car_Dekho_Property.py
@@ -3,8 +3,9 @@ from odoo import fields, models
 class CarDekhoProperty(models.Model):
      _name="car.dekho.property"
      _description="Car available at cheapest price"
-
-     car_name = fields.Char(required=True)
+     
+     _rec_name="car_name"
+     car_name = fields.Char('Name',required=True)
      description = fields.Char()
      car_code=fields.Char()
      license_number=fields.Char(required=True)
@@ -30,5 +31,8 @@ class CarDekhoProperty(models.Model):
                               required=True,
                               copy=False,
                               default="new")
-
-
+     car_property_id = fields.Many2one("cardekho.property.type",string="Car Type")
+     user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+     buyer_id = fields.Many2one("res.partner", string="Buyer", readonly=True, copy=False)
+     tag_ids = fields.Many2many("cardekho.property.tag", string="Tags")
+     offer_ids = fields.One2many("cardekho.property.offer", "car_id", string="Offers")
