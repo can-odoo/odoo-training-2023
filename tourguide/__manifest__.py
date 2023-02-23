9c73e6357dcd8f5981019a63456ce117aef7d5fd
Jain Patel (japa) <japa@odoo.com>
2023-02-23 18:15:13 +0530

    [Chapter 8]:change a position of tour menus in tour_guide_view file

--- tourguide/__manifest__.py
+++ tourguide/__manifest__.py
@@ -3,6 +3,8 @@
     'data' : [
         'security/ir.model.access.csv',
         'views/tour_guide_views.xml',
+        'views/tour_packages_views.xml',
+        'views/tour_tag_views.xml',
         # 'views/tour_menus.xml'
     ]
 }
