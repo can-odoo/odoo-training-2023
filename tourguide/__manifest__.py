7093e60bf13dd60ed27f0fd30d7531c9731650c1
Jain Patel (japa) <japa@odoo.com>
2023-02-23 18:18:38 +0530

    [Chapter 8]: added view folder path of tour_tag,tour_packages in menifest file

--- tourguide/__manifest__.py
+++ tourguide/__manifest__.py
@@ -5,6 +5,6 @@
         'views/tour_guide_views.xml',
         'views/tour_packages_views.xml',
         'views/tour_tag_views.xml',
-        # 'views/tour_menus.xml'
+        #'views/tour_menus.xml'
     ]
 }
