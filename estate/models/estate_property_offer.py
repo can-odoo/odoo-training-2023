45c3064c156a521b159b1e98932c56db6a8d836f
Jain Patel (japa) <japa@odoo.com>
2023-02-27 18:27:58 +0530

    [Chapter 9]: added validity, date_deadline field and use compute and inverse method for date_deadline

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import datetime, timedelta
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -9,3 +10,20 @@ class EstatePropertyOffer(models.Model):
         selection = [('accepted', 'Accepted'),('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer('Validity(Days)', default=7)
+    date_deadline = fields.Date('Deadline', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for data in self:
+            date = data.create_date.date() if data.create_date else fields.Date.today()
+            data.date_deadline = date + timedelta(days = data.validity)
+    
+    def _inverse_date_deadline(self):
+        for data in self:
+            date = data.create_date.date() if data.create_date else fields.Date.today()
+            data.validity = (data.date_deadline - date).days
+            
+
+
+
