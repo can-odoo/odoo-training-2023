569b5d4de538c443fc9f0cea6c9e3c98d5c6bdf5
utag-odoo <utag@odoo.com>
2023-02-27 18:18:48 +0530

    chapter : 9,inverse function added
    add new parameter validity and date_deadline
    implement inverse function for date_deadline using two fields 'create_date and validity'

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from dateutil.relativedelta import relativedelta
 
 class propertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -16,4 +17,16 @@ class propertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.recurring.plan", string="Property", required=True)
-    
+    validity = fields.Integer("Validity",default=7)
+    date_deadline = fields.Date("Deadline", compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends("create_date","validity")
+    def _compute_date_deadline(self):
+        for i in self:
+            date = i.create_date.date() if i.create_date else fields.Date.today()
+            i.date_deadline = date + relativedelta(days=i.validity)
+
+    def _inverse_date_deadline(self):
+        for i in self:
+            date = i.create_date.date() if i.create_date else fields.Date.today()
+            i.validity = (i.date_deadline - date).days
