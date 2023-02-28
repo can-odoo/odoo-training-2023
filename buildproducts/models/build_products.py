0bb3af1757a8e28b9df8a21a1a7fb42957d68c19
radh-odoo <radh@odoo.com>
2023-02-28 18:27:17 +0530

    [Chapter 9]: added total_sale compute field and onchange method

--- buildproducts/models/build_products.py
+++ buildproducts/models/build_products.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from odoo import api
 
 class BuildProducts(models.Model):
     _name = "build.products"
@@ -13,3 +14,19 @@ class BuildProducts(models.Model):
     active = fields.Boolean(string='Active', default=True)
     order_ids = fields.One2many('build.products.order', 'product_id', string='Orders')
     tag_ids = fields.Many2many('build.products.tag' , string='Product Tags')
+    total_sale = fields.Float(string='Total Sales' , compute='_compute_total_sales')
+
+    @api.onchange('order_ids')
+    def _onchange_order_qty(self):
+        for product in self:
+            ordered_qty = sum(order.qty_ordered for order in product.order_ids)
+            if ordered_qty:
+                product.product_quantity = product._origin.product_quantity - ordered_qty
+
+    @api.depends('order_ids')
+    def _compute_total_sales(self):
+        for record in self:
+            if record.order_ids:
+                record.total_sale = sum(record.order_ids.mapped('total_amount'))
+            else:
+                record.total_sale=0.0
