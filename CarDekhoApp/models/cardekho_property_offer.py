147ae08bcef18c13cbbd4079430b3e8ef4f460c0
utag-odoo <utag@odoo.com>
2023-02-28 18:12:08 +0530

    implement compute and inverse funtion to calculate date_deadline

--- CarDekhoApp/models/cardekho_property_offer.py
+++ CarDekhoApp/models/cardekho_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from dateutil.relativedelta import relativedelta
 
 class carOffers(models.Model):
     _name="cardekho.property.offer"
@@ -16,3 +17,15 @@ class carOffers(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Offer From", required=True)
     car_id = fields.Many2one("car.dekho.property", string="CarId", required=True)
+    validity = fields.Integer("Validity",default=4)
+    date_deadline  = fields.Date("Deadline", compute='_date_deadline', inverse='_inverse_deadline')
+    
+    @api.depends("create_date","validity")
+    def _date_deadline(self):
+        for i in self:
+            date = i.create_date.date() if i.create_date else fields.Date.today()
+            i.date_deadline = date + relativedelta(days=i.validity)
+    def _inverse_deadline(self):
+        for i in self:
+            date = i.create_date.date() if i.create_date else fields.Date.today()
+            i.validity = (i.date_deadline - date).days
