516f02de9ce871281953d9634a6cfba848248473
divy-odoo <divy@odoo.com>
2023-03-01 10:57:08 +0530

    [ Chapter 9 ] : Added compute method for revanue & onchange for store availability

--- franchise_ERP/models/franchise_store_property.py
+++ franchise_ERP/models/franchise_store_property.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import models,fields,api
 
 class FranchiseStoreProperty(models.Model):
     _name = "franchise.store.property"
@@ -11,14 +11,26 @@ class FranchiseStoreProperty(models.Model):
     no_of_product = fields.Integer(default=0)
     location = fields.Char()
     opening_date = fields.Date(default = fields.Date.today)
-    sale = fields.Float(copy=False)
-    revanue = fields.Float("Total revanue :",readonly=True,default=True,copy=False) 
+    sale = fields.Float("Net sale ", copy=False)
+    profit_margin = fields.Float("Profit Margin ( % )")
+    net_revanue = fields.Float("Total revanue ( rs ) :",readonly=True,default=0,copy=False,compute="_compute_revanue") 
     phone_no =fields.Char(size=10)
     no_of_employee = fields.Integer()
     # type_franchise = fields.Selection(selection = [('job franchise','job franchise'),('product franchise','product franchise'),('business format franchise','business format franchise'),('investment franchise','investment franchise'),('conversion franchise','conversion franchise')])
-    available = fields.Boolean("Active")
+    available = fields.Boolean("Active",default=True)
     product_ids = fields.One2many('franchise.product.stock','franchise_id')
 
 
+    @api.depends('sale','profit_margin')
+    def _compute_revanue(self):
+        for store in self:
+            store.net_revanue = store.sale * store.profit_margin/100
 
 
+    @api.onchange('available')
+    def _onchange_available(self):
+        for store in self:
+            if store.available==False:
+                return {'warning': {
+                'title': ("Warning"),
+                'message': ('Store closed !!')}}
