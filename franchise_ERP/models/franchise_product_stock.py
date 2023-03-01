40bc5170b0bbc38b5542a2e52c08a59694e911d7
divy-odoo <divy@odoo.com>
2023-03-01 14:30:29 +0530

    [ Chapter 9 ] : added onchange_quantity & compute_price method

--- franchise_ERP/models/franchise_product_stock.py
+++ franchise_ERP/models/franchise_product_stock.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import models,fields,api
 
 class FranchiseProductStock(models.Model):
     _name = "franchise.product.stock"
@@ -7,4 +7,20 @@ class FranchiseProductStock(models.Model):
     product_id = fields.Many2one('franchise.product')
     franchise_id = fields.Many2one('franchise.store.property')
     quantity = fields.Integer()
-    status = fields.Selection(selection=[('In stock','in_stock'),('out of stock','Out of Stock'),('ordered', 'Ordered')])
+    status = fields.Selection(selection=[('in_stock','In stock'),('out of stock','Out of Stock'),('ordered', 'Ordered')])
+    price = fields.Integer("Price",compute="_compute_price")
+
+    @api.onchange('quantity')
+    def _onchange_quantity(self):
+        for product in self:
+            if product.quantity == 0:
+                product.status = "out of stock"
+            elif product.quantity>0 :
+                product.status = "in_stock"
+
+    @api.depends('quantity','product_id','status')
+    def _compute_price(self):
+        for product in self:
+            product.price = (product.product_id.price * product.quantity)
+            if product.status=='ordered':
+                product.price = (-1)*product.price
