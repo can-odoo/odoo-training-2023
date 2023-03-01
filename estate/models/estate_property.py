7bd90ecdf3101002919fd499c80f1f558ff8f3a6
divy-odoo <divy@odoo.com>
2023-03-01 10:54:53 +0530

    [ Chapter 9 ] : added compute method for total_area & best_offer & onchange for garden

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import api,models,fields
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -24,3 +24,28 @@ class EstateProperty(models.Model):
     salesman = fields.Many2one('res.users',string="Salesman", default=lambda self: self.env.user)
     buyer = fields.Many2one('res.partner',copy =False)
     offer_ids = fields.One2many('estate.property.offer','property_id')
+    total_area = fields.Float('Total area ( sqm )',compute="_compute_total")
+    best_offer = fields.Float("Best offer",compute="_compute_bestoffer")
+
+
+    @api.depends('garden_area','living_area')
+    def _compute_total(self):
+        for record in self:
+            record.total_area = record.garden_area+record.living_area
+
+    @api.depends('offer_ids')
+    def _compute_bestoffer(self):
+        for record in self: 
+            if record.offer_ids:    
+                self.best_offer = max(record.offer_ids.mapped('price'))
+            else:
+                self.best_offer = 0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden == True:
+            self.garden_orientation="north"
+            self.garden_area = 10
+        else:
+            self.garden_area = 0
+            self.garden_orientation=None
