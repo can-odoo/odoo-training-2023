a0c8d7984c1ad809b824fc73d24c1ba52b4db6bb
divy-odoo <divy@odoo.com>
2023-03-01 10:56:06 +0530

    [ Chapter 9 ] : added compute & inverse method for offer deadline & validity

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import api,models,fields
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -8,3 +8,23 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(selection =[('accepted','Accepted'),('refused','Refused')], copy=False)
     partner_id = fields.Many2one('res.partner',required = True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer("Validity ( days )",default=7)
+    deadline = fields.Date("Deadline",compute="_compute_deadline",default = fields.date.today(),inverse="_inverse_deadline")
+
+
+    @api.depends('validity','create_date')
+    def _compute_deadline(self):
+        for record in self:
+            print(record.create_date)
+            if record.create_date:
+                record.deadline = fields.Date.add(record.create_date.date(),days=record.validity)
+            else :
+                record.deadline = fields.Date.add(fields.date.today(),days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.validity = (record.deadline-record.create_date.date()).days
+            else :
+                record.validity = (record.deadline-fields.date.today()).days
+                            
