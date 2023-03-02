dd96a5a3c46247d6fdde1419854137da28c2a9e6
radh-odoo <radh@odoo.com>
2023-03-02 18:07:36 +0530

    [Chapter 11]: added sql unique constraint for name field

--- buildproducts/models/build_products_tag.py
+++ buildproducts/models/build_products_tag.py
@@ -5,3 +5,7 @@ class BuildProductsTag(models.Model):
     _description = 'Build Products Tag'
 
     name = fields.Char('Products Tags', required=True)
+
+    _sql_constraints = [
+        ('check_products_tags', 'UNIQUE(name)', 'Product Tag already exists.')
+    ]
