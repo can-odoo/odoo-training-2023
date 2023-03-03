dd96e2e111e3d47cd2fddb7586fad8767e0447fd
divy-odoo <divy@odoo.com>
2023-03-03 18:35:15 +0530

    [ Chapter 12 ] : added attribute to display list order by name

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,7 +3,10 @@ from odoo import models,fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "This is property type"
+    _order = "sequence,name"
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many('estate.property','property_type_id')
+    sequence = fields.Integer('Sequence')
 
     _sql_constraints = [ ('check_property_type','UNIQUE (name)','Property Type should be unique')]
