4fa45bc2e617a6a795b5b2fe8e59b2df32f43c10
Jain Patel (japa) <japa@odoo.com>
2023-03-03 18:44:16 +0530

    [Chapter 12]: added _order based on name and added new sequence field and one2many property_ids field

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,8 +3,12 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Real Estate Property Types'
+    _order = "sequence, name"
 
     name = fields.Char('Property Types', required=True)
+    sequence = fields.Integer('Sequence', help="Used to order stages. Lower is better.")
+
+    property_ids = fields.One2many('estate.property','property_type_id')
 
     #sql constraints
     _sql_constraints = [
