37b0d0f7103789de722639d8a9a5821d6d7fa91b
divy-odoo <divy@odoo.com>
2023-03-03 18:32:50 +0530

    [ Chapter 20 ] : added attribute to display list order by name

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -3,7 +3,9 @@ from odoo import models,fields
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "This is property tag"
+    _order = "name"
 
     name = fields.Char(required=True)
+    color = fields.Integer()
 
     _sql_constraints = [ ('check_property_tag','UNIQUE (name)','Property Tag should be unique')]
