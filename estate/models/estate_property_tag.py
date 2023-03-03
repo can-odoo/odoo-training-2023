a1eb61a21b2f339759e685c04632a4b8f6b73c55
utag-odoo <utag@odoo.com>
2023-03-03 18:17:14 +0530

    [ chapter 12 ] : set determinstic order of records based on'name' and add 'color' field.

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -3,6 +3,8 @@ from odoo import fields, models
 class PropertyTags(models.Model):
     _name="estate.property.tag"
     _description="Estate Property Tag"
+    _order="name"
 
     name = fields.Char('Property Tag',required=True)
+    color = fields.Integer('Color')
     _sql_constraints = [('Tagname','UNIQUE(name)','Property tag name must be unique')]
