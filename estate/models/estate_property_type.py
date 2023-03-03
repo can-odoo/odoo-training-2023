95939b107eb53b96720d98cc0fc8a6d2dd92434f
utag-odoo <utag@odoo.com>
2023-03-03 18:17:59 +0530

    [ chapter 12 ] : set determinstic order of records based on'name'

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,7 +3,10 @@ from odoo import fields, models
 class PropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Property type are defined"
-
+    _order = "sequence, name"
+ 
     name = fields.Char('Property Type',required=True)
+    property_ids = fields.One2many('estate.recurring.plan','property_type_id',string='Properties')
+    sequence = fields.Integer('Sequence', default=10)
     _sql_constraints = [('Typename','UNIQUE(name)','Property type name must be unique')]
     
