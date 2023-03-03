ac8879d93582f164c1ea248987d0861a5cb0d13b
radh-odoo <radh@odoo.com>
2023-03-03 18:15:20 +0530

    [Chapter 12]: added _order field and properties_ids & sequence field

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,8 +3,11 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Real Estate Property Type" 
+    _order = "name , sequence"
 
     name = fields.Char('Property Type' , required=True)
+    properties_ids = fields.One2many('estate.property', 'property_type_id', string='Properties')
+    sequence = fields.Integer('Sequence')
 
     _sql_constraints = [
         ('unique_property_type', 'UNIQUE(name)', 'Property Type already exists.')
