69faa2050871caeb3f84d059afaf882cdae7b1a9
Jain Patel (japa) <japa@odoo.com>
2023-03-03 18:41:18 +0530

    [Chapter 12]: added _order based on name and added new color field

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -3,8 +3,10 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = 'estate.property.tag'
     _description = 'Estate Properties Tags'
+    _order = "name"
 
     name = fields.Char('Property Tags', required=True)
+    color = fields.Integer('Color')
 
     # sql constraints
     _sql_constraints = [
