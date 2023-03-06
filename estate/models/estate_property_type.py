d0b28ea1a14c6ce96dbd31749536c94cb1b26870
Jain Patel (japa) <japa@odoo.com>
2023-03-06 18:50:30 +0530

    [Chapter 12]: added two new fields offer_ids, offer_count and one compute method

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
@@ -9,8 +9,16 @@ class EstatePropertyType(models.Model):
     sequence = fields.Integer('Sequence', help="Used to order stages. Lower is better.")
 
     property_ids = fields.One2many('estate.property','property_type_id')
+    offer_ids = fields.One2many('estate.property.offer','property_type_id')
+    offer_count = fields.Integer(compute="_offer_count")
+
 
     #sql constraints
     _sql_constraints = [
         ('check_property_type','UNIQUE(name)', 'The name must be unique')
     ]
+
+    @api.depends('offer_ids')
+    def _offer_count(self):
+        for data in self:
+            data.offer_count = len(data.offer_ids)
