d01147e47b90d27711b2670bbf06dab6ed7b6fa4
utag-odoo <utag@odoo.com>
2023-03-06 18:02:31 +0530

    [ chapter 12 ] : add two fields 'offer_ids,offer_count' and add a compute function on offer_count

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class PropertyType(models.Model):
     _name = "estate.property.type"
@@ -8,5 +8,11 @@ class PropertyType(models.Model):
     name = fields.Char('Property Type',required=True)
     property_ids = fields.One2many('estate.recurring.plan','property_type_id',string='Properties')
     sequence = fields.Integer('Sequence', default=10)
-    _sql_constraints = [('Typename','UNIQUE(name)','Property type name must be unique')]
-    
+    offer_ids = fields.One2many('estate.property.offer','property_type_id', string='Offers')
+    offer_count=fields.Integer(compute="_count_offers", string='Offer Count')
+    _sql_constraints = [('Typename','UNIQUE(name)','Property type name must be unique')]    
+
+    @api.depends('offer_ids')
+    def _count_offers(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
