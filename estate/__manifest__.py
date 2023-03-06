8f7d9b8503c2b45b571c1ae409d220c5e202489b
divy-odoo <divy@odoo.com>
2023-03-06 17:21:46 +0530

    [ Updates ] : Changed the order of file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,9 +6,9 @@
 		'security/ir.model.access.csv', 
         
 		'views/estate_property_view.xml',
+        'views/estate_property_offer_view.xml',
 		'views/estate_property_type_view.xml',
 		'views/estate_property_tag_view.xml',
-        'views/estate_property_offer_view.xml',
 		# 'views/estate_menus.xml'
 	],
     'application' : True
