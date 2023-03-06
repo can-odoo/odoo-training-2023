a9af4c477381c9d519945e4d9a1c541d18616282
utag-odoo <utag@odoo.com>
2023-03-06 18:38:45 +0530

    [ chapter 12 ] : implements list ordering using'_order' and add new relationa fields.

--- CarDekhoApp/models/cardekho_property_type.py
+++ CarDekhoApp/models/cardekho_property_type.py
@@ -1,8 +1,17 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class PropertyType(models.Model):
     _name = "cardekho.property.type"
     _description = "Car property described here"
+    _order="name"
 
     name = fields.Char('Car type',required=True)
-    
+    property_ids = fields.One2many('car.dekho.property','car_property_id',string='Properties')
+    offer_ids = fields.One2many('cardekho.property.offer','car_property_id',string='Offers')
+    offer_count = fields.Integer(compute="_count_offer", string='Offer Count')
+    _sql_constraints = [('Typename','UNIQUE(name)','Car type name must be unique')]
+
+    @api.depends("offer_ids")
+    def _count_offer(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
