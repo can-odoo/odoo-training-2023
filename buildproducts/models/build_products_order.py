d5b09e94fe01322e69ec5171a2ace17d66b3381c
radh-odoo <radh@odoo.com>
2023-03-07 16:52:06 +0530

    [Chapter 12]: added _order field and removed status field

--- buildproducts/models/build_products_order.py
+++ buildproducts/models/build_products_order.py
@@ -1,9 +1,11 @@
 from odoo import models, fields
 from odoo import api
+from odoo.exceptions import ValidationError, UserError
 
 class BuildProductsOrder(models.Model):
     _name = 'build.products.order'
     _description = 'Build Products Order'
+    _order = 'total_amount desc'
     
     buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
     order_date = fields.Date(string='Order Date', default=fields.Date.today())
@@ -12,16 +14,11 @@ class BuildProductsOrder(models.Model):
     delivery_date = fields.Date('Expected Delivery Date' , default=fields.Date.add(fields.Date.today(), days=2))
     state = fields.Selection([
         ('new', 'New'),
-        ('order_accepted', 'Order Accepted'),
         ('in_progress', 'In Progress'),
         ('in_delivery', 'In Delivery'),
         ('delivered', 'Delivered'),
         ('canceled', 'Canceled')
     ], string='State', default='new')
-    status = fields.Selection([
-        ('accepted' , 'Accepted'),
-        ('refused' , 'Refused')
-    ], string='Status', required=True)
     product_id = fields.Many2one('build.products', string='Product', required=True)
     total_amount = fields.Float(compute='_compute_total_amount' , inverse='_inverse_total_amount' , string='Total Amount' , store=True)
 
@@ -34,27 +31,44 @@ class BuildProductsOrder(models.Model):
         for order in self:
             order.qty_ordered = order.total_amount / order.product_id.product_price
 
-    def action_confirmed(self):
+    def action_canceled(self):
         for order in self:
-            order.state = 'order_accepted'
+            if order.state == 'delivered':
+                raise UserError('Delivered Order cannot be marked canceled')
+            order.state = 'canceled'
+            # # order.product_id.product_quantity += order.qty_ordered
+            order.total_amount = 0
+            # if order.product_id:
+            #     order.product_id.product_quantity += order.qty_ordered
+            #     order.product_id.total_sale -= order.total_amount
+            # this will not work because afterwards when a new record is created then it will again add the total_amount to total_sale.
 
     def action_delivered(self):
         for order in self:
+            if order.state == 'canceled':
+                raise UserError('Canceled order cannot be marked delivered')
             order.state = 'delivered'
 
-    # def write(self, vals):
-    #     if 'qty_ordered' in vals and self.product_id:
-    #         self.product_id.product_quantity -= vals['qty_ordered']
-    #     return super(BuildProductsOrder, self).write(vals)
+    @api.constrains('total_amount', 'product_id.product_price')
+    def _check_total_amount(self):
+        for order in self:
+            if order.total_amount <= order.product_id.product_price:
+                raise ValidationError('Total Amount cannot be less than Product Price!')
+
+    def create(self, vals):
+        order = super(BuildProductsOrder, self).create(vals)
+        order.product_id.product_quantity -= order.qty_ordered
+        return order
     
-    # def write(self, vals):
-    #     if 'qty_ordered' in vals and self.product_id:
-    #         qty_diff = vals['qty_ordered'] - self.qty_ordered
-    #         self.product_id.product_quantity -= qty_diff
-    #     return super(BuildProductsOrder, self).write(vals)
+    def write(self, vals):
+        if 'qty_ordered' in vals and self.product_id:
+            qty_diff = vals['qty_ordered'] - self.qty_ordered
+            self.product_id.product_quantity -= qty_diff
+        return super(BuildProductsOrder, self).write(vals)
     
-    # def unlink(self):
-    #     for order in self:
-    #         if order.product_id:
-    #             order.product_id.product_quantity += order.qty_ordered
-    #     return super(BuildProductsOrder, self).unlink()
+    def unlink(self):
+        for order in self:
+            if order.product_id:
+                order.product_id.product_quantity += order.qty_ordered
+        return super(BuildProductsOrder, self).unlink()
+   
