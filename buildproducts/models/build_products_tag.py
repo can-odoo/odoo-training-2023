893d1dd5923ae60a77b0b5cd5917cba19c9be33a
radh-odoo <radh@odoo.com>
2023-03-07 16:52:39 +0530

    [Chapter 12]: added color field

--- buildproducts/models/build_products_tag.py
+++ buildproducts/models/build_products_tag.py
@@ -5,6 +5,7 @@ class BuildProductsTag(models.Model):
     _description = 'Build Products Tag'
 
     name = fields.Char('Products Tags', required=True)
+    color = fields.Integer('Color')
 
     _sql_constraints = [
         ('check_products_tags', 'UNIQUE(name)', 'Product Tag already exists.')
