d380c251187367757c0b8ecc7de161070283b609
divy-odoo <divy@odoo.com>
2023-03-07 18:03:36 +0530

    [ Chapter 12 ] : added color attribute & order by name

--- franchise_ERP/models/franchise_product.py
+++ franchise_ERP/models/franchise_product.py
@@ -3,6 +3,7 @@ from odoo import models,fields
 class FranchiseProduct(models.Model):
     _name = "franchise.product"
     _description = "This is Product model contains the information about product"
+    _order = "name"
 
     name = fields.Char()
     price = fields.Float()
