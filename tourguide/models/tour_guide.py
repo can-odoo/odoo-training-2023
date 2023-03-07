6dd5ef660095319e8bcf4dfe78a8dc9bd019344b
Jain Patel (japa) <japa@odoo.com>
2023-03-07 18:10:55 +0530

    added and removed some fields for modification

--- tourguide/models/tour_guide.py
+++ tourguide/models/tour_guide.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from datetime import datetime, timedelta
 
 class TourGuide(models.Model):
     _name = 'tour.guide'
@@ -6,19 +7,20 @@ class TourGuide(models.Model):
 
     name = fields.Char('Tour Name')
     description = fields.Char('Tour Description')
-    # duration = fields.Integer('Duration(km)')
-    # cost = fields.Float('cost',readonly=True)
+    price = fields.Float('Price')
     destination = fields.Char('Place Name')
+    duration = fields.Integer('Duration',default = 6, compute="_compute_duration", inverse="_set_duration", store=True)
+    desc = fields.Text("Desc")
     tour_type = fields.Selection(
         selection = [('group', 'Group'), ('solo', 'Solo'), ('family', 'Family')]
     )
+    status = fields.Selection(
+        selection = [('available', 'Available'), ('unavailable', 'Unavailable')], 
+    )
     start_date = fields.Date('Start Date')
     end_date = fields.Date('End Date')
-    active = fields.Boolean(string="Active",default=True)
-    min_price = fields.Float("Min_Price", compute="_check_min_price")
-    max_price = fields.Float("Max_Price", compute="_check_max_price")
-    package_ids = fields.One2many('tour.packages','tour_ids')
     tag_ids = fields.Many2many('tour.tag')
+    guide_id = fields.Many2one('res.users', string="TourGuide", default=lambda self:self.env.user)
 
     @api.depends('package_ids.price')
     def _check_min_price(self):
@@ -35,4 +37,19 @@ class TourGuide(models.Model):
             else:
                 self.max_price = 0
 
-                
+    @api.depends('start_date','end_date')
+    def _compute_duration(self):
+        for data in self:
+            if data.start_date and data.end_date:
+                data.duration = (data.end_date - data.start_date).days
+    
+    def _set_duration(self):
+        for data in self:
+            if data.start_date and data.duration:
+                data.end_date = data.start_date + timedelta(days = data.duration)
+    
+    #sql constraints
+    _sql_constraints = [
+        ('check_price', 'CHECK(price > 0)', 'Tour Price must be Stricly Positive'),
+        ('check_duration', 'CHECK(duration > 0)', 'Duration should be positive')
+    ]
