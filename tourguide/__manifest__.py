4fb79d373602493aada868198560c549928889a2
Jain Patel (japa) <japa@odoo.com>
2023-03-07 18:07:58 +0530

    added booking view and removed package view

--- tourguide/__manifest__.py
+++ tourguide/__manifest__.py
@@ -2,8 +2,8 @@
     'name' : 'Tour Guide',
     'data' : [
         'security/ir.model.access.csv',
+        'views/tour_booking_views.xml',
         'views/tour_guide_views.xml',
-        'views/tour_packages_views.xml',
         'views/tour_tag_views.xml',
         #'views/tour_menus.xml'
     ]
