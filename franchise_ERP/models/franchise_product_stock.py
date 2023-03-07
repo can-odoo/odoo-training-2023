93bd24bdef0ece9d000252c3624cf49bd3fe69f1
divy-odoo <divy@odoo.com>
2023-03-07 18:07:22 +0530

    [ Updates ] : added name field and compute function

--- franchise_ERP/models/franchise_product_stock.py
+++ franchise_ERP/models/franchise_product_stock.py
@@ -4,11 +4,12 @@ class FranchiseProductStock(models.Model):
     _name = "franchise.product.stock"
     _description = "This model stores the details of stocks of perticular stores"
 
+    name = fields.Char('Name',compute='_compute_name')
     product_id = fields.Many2one('franchise.product')
     franchise_id = fields.Many2one('franchise.store.property')
     quantity = fields.Integer()
     status = fields.Selection(selection=[('in_stock','In stock'),('out of stock','Out of Stock'),('to_order','To order'),('ordered', 'Ordered')])
-    price = fields.Float("Price",compute="_compute_price")
+    price = fields.Float("Price",compute="_compute_price",store=True)
 
     @api.onchange('quantity')
     def _onchange_quantity(self):
@@ -17,13 +18,18 @@ class FranchiseProductStock(models.Model):
                 product.status = "out of stock"
             elif product.quantity>0 :
                 product.status = "in_stock"
+            else:
+                product.status="to_order"
 
-    @api.depends('quantity','product_id','status')
+    @api.depends('quantity','product_id')
     def _compute_price(self):
-        for product in self:
+        for product in self:                                     
             product.price = (product.product_id.price * product.quantity)
-            if product.status=='ordered':
-                product.price = (-1)*product.price
+
+    @api.depends('product_id')
+    def _compute_name(self):
+        for record in self:
+            record.name = record.franchise_id.name + " - " + record.product_id.name
 
     def action_order(self):
         for product in self:
