abe950ec849e6ca54816cdd1124f399cc2180e1e
Jain Patel (japa) <japa@odoo.com>
2023-03-13 18:49:51 +0530

    [Chapter 14]: modified a file

--- tourguide/__manifest__.py
+++ tourguide/__manifest__.py
@@ -2,9 +2,10 @@
     'name' : 'Tour Guide',
     'data' : [
         'security/ir.model.access.csv',
-        'views/tour_booking_views.xml',
         'views/tour_guide_views.xml',
+        'views/tour_booking_views.xml',
         'views/tour_tag_views.xml',
+        'views/res_users_views.xml',
         #'views/tour_menus.xml'
     ]
 }
