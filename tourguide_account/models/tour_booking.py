ea829d09b359d88004f332e9b2b8e4d0f7c79cd5
Jain Patel (japa) <japa@odoo.com>
2023-03-13 18:56:22 +0530

    [Chapter 14]: create two new records in account.move model

--- tourguide_account/models/tour_booking.py
+++ tourguide_account/models/tour_booking.py
@@ -1,8 +1,25 @@
-from odoo import fields, models
+from odoo import fields, models, Command
 
 class TourBooking(models.Model):
     _inherit='tour.booking'
 
     def action_to_confirm(self):
-        print("invoice generate1212121")
+        print("invoice generate")
+        self.env["account.move"].create(
+            {
+                'move_type': 'out_invoice',
+                'invoice_line_ids': [
+                    Command.create({
+                        "name": "customer",
+                        "quantity": 1,
+                        "price_unit": self.total_cost
+                    }),
+                    Command.create({
+                        "name": "Administrative Fees",
+                        "quantity": 1,
+                        "price_unit": 100
+                    })
+                ]
+            }
+        )
         return super(TourBooking, self).action_to_confirm()
