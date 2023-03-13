06ba7f42779cc97cb0a2ca35ee5fcaf76153e13f
Jain Patel (japa) <japa@odoo.com>
2023-03-13 18:52:07 +0530

    [Chapter 14]: modified a file and change some fields value

--- tourguide/models/tour_booking.py
+++ tourguide/models/tour_booking.py
@@ -1,5 +1,5 @@
 from odoo import fields, models, api
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError,UserError
 
 class TourBooking(models.Model):
     _name = 'tour.booking'
@@ -10,14 +10,13 @@ class TourBooking(models.Model):
     booking_status = fields.Selection([
         ('draft', 'Draft'),
         ('confirmed', 'Confirmed'),
-        ('cancelled', 'Cancelled'),
+        ('canceled', 'Canceled'),
     ], string='Booking Status', default='draft', required=True)
     total_cost = fields.Float('Total Cost', compute="_total_cost", store=True)
     # tour_name = fields.Char('Tour',store=True)
     tour_guide_id = fields.Many2one('tour.guide', string='Tour', store=True)
     guide = fields.Char('Tour Guide')
     ticket_qty = fields.Integer(string='Ticket for Multiple Persons', default=1)
-
 
     @api.onchange('tour_guide_id')
     def onchange_tour_guide_id(self):
@@ -39,3 +38,23 @@ class TourBooking(models.Model):
                 raise ValidationError("Booking date cannot be after tour start date.")
             if booking.booking_status == 'confirmed' and booking.tour_guide_id and booking.booking_date > booking.tour_guide_id.end_date:
                 raise ValidationError("Booking date cannot be after tour end date.")
+    
+    @api.ondelete(at_uninstall=False)
+    def _check_status(self):
+        for data in self:
+            if data.booking_status == 'confirmed':
+                raise ValidationError("Confirmed Booking can not be canceled")
+    
+    def action_to_confirm(self):
+        for data in self:
+            if (data.booking_status == 'canceled'):
+                raise UserError("Canceled booking can not be confirmed")
+            data.booking_status = 'confirmed'
+        return True
+    
+    def action_to_cancel(self):
+        for data in self:
+            if (data.booking_status == 'Confirmed'):
+                raise UserError("Confirmed booking can not be canceled")
+            data.booking_status = 'canceled'
+        return True
