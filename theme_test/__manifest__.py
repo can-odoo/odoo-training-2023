8c6a1f7517309197ae129105044bbb9345cc8afb
divy-odoo <divy@odoo.com>
2023-03-20 18:51:30 +0530

    [ Theme ] : added  test theme & snippets

---
+++ theme_test/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Test theme',
+    'description': 'This is my first theme',
+    'version': '1.0',
+    'author': 'Divyesh vyas',
+    'category': 'Theme/creative',
+    'depends': ['website'],
+
+    'data': [
+        'views/assets.xml',
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/snippets.xml',
+        'views/options.xml'
+
+
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'theme_test/static/scss/style.scss',
+        ] 
+    }
+}
