e3307639e008aad74d149dca878734923eef96c0
Jain Patel (japa) <japa@odoo.com>
2023-03-21 18:53:36 +0530

    added a path to manifest file of newly added xml file

--- theme_tutorial/__manifest__.py
+++ theme_tutorial/__manifest__.py
@@ -9,11 +9,16 @@
     'data': [
         'views/assets.xml',
         'views/layout.xml',
-        'views/pages.xml'
+        'views/pages.xml',
+        'views/snippets.xml',
+        'views/options.xml'
     ],
     'assets': {
         'web.assets_frontend': [          
             'theme_tutorial/static/scss/style.scss'
+        ],
+        'website.assets_editor': [
+            'theme_tutorial/static/js/tutorial_editor.js'
         ]
     }
 }
