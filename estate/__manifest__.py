1071ed860e2544892a65a0eddecbfa1792cc1477
divy-odoo <divy@odoo.com>
2023-03-23 18:48:53 +0530

    [ Updates ] : created websited template

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,6 +2,7 @@
 	'name':'Real Estate',
 	'summary': 'Track leads and close opportunities',
 	'version' : '1.2',
+	'depends':['website'],
     'data':[
 		'security/ir.model.access.csv', 
         
@@ -10,6 +11,7 @@
 		'views/estate_property_type_view.xml',
 		'views/estate_property_tag_view.xml',
         'views/res_users_view.xml',
+        'views/website_template.xml',
         'report/estate_property_report.xml',
         'report/estate_property_template.xml',
         'report/res_user_report.xml',
