11ea2394c9a547c5554f4dabb2cd68ca4888ba1f
divy-odoo <divy@odoo.com>
2023-03-24 18:57:46 +0530

    [ Controllers ] : added path of controller

--- franchise_ERP/__manifest__.py
+++ franchise_ERP/__manifest__.py
@@ -1,16 +1,17 @@
 {
 	'name':'Franchise Manager',
 	'summary':'Hey! I am your showroom manager, Lets have a managed showroom',
+    'depends':['website'],
     'data':[
 		'security/ir.model.access.csv', 
         
+        'views/website_template.xml',
 		'views/franchise_store_property_view.xml',
         'views/franchise_product_type_view.xml',
         'views/franchise_product_view.xml',
         'views/franchise_product_order_view.xml',
 		'views/franchise_product_stock_view.xml',
 		'views/franchise_menus.xml',
-  
 	]
 
 }
