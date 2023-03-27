d2d550ac3e2c7fab79f019171e32f8a649af2383
Jain Patel (japa) <japa@odoo.com>
2023-03-27 18:48:48 +0530

    modified a file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,19 +1,20 @@
 {
-	'name': 'Real Estate',
-    
-  'depends': ['website'],
-  'data' : [
-	'security/ir.model.access.csv',
-  'views/estate_property_views.xml',
-  'views/estate_property_offer_views.xml',
-  'views/estate_property_type_views.xml',
-  'views/estate_property_tag_views.xml',
-  'views/res_users_views.xml',
-  'views/web_pages.xml',
-  'views/page.xml',
-  'report/estate_property_templates.xml',
-  'report/estate_property_reports.xml',
-      # 'views/estate_menus.xml'
-	],
-    
-} 
+    'name': 'Real Estate',
+
+    'depends': ['website'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/set_offer_views.xml',
+        'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/res_users_views.xml',
+        'views/web_pages.xml',
+        'views/page.xml',
+        'report/estate_property_templates.xml',
+        'report/estate_property_reports.xml',
+        # 'views/estate_menus.xml'
+    ],
+
+}
