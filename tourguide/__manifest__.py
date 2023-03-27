7b40e16a42a0ad5f29d6e14976e6c585368d3eea
Jain Patel (japa) <japa@odoo.com>
2023-03-27 18:53:49 +0530

    added view and depend path

--- tourguide/__manifest__.py
+++ tourguide/__manifest__.py
@@ -1,11 +1,14 @@
 {
     'name' : 'Tour Guide',
+    'depends': ['website'],
     'data' : [
         'security/ir.model.access.csv',
         'views/tour_guide_views.xml',
         'views/tour_booking_views.xml',
         'views/tour_tag_views.xml',
         'views/res_users_views.xml',
+        'views/tour_page.xml',
+        'views/web_tour_page.xml',
         #'views/tour_menus.xml'
     ]
 }
