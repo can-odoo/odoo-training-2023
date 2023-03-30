fe49c8c3fdebe71e3424bed55e9313da4dd3e9a7
radh-odoo <radh@odoo.com>
2023-03-30 18:39:25 +0530

    added depends, and pages and templates view file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,8 @@
 {
 	'name':'Real Estate',
 	'summary':'Hello! This is an E-state App.',	
+    
+	'depends':['website'],
 
 	'data' : [
 
@@ -10,6 +12,8 @@
 		'views/estate_property_type_views.xml',
 		'views/estate_property_tag_views.xml',
         'views/res_users_views.xml',
+        'views/pages.xml',
+        'views/templates.xml',
         'reports/estate_property_reports.xml',
         'reports/estate_property_templates.xml',
 		# 'views/estate_menus.xml'
